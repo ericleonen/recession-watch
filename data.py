f1017d1da45df7046fe99551c42c1bf30c942b7c
ericleonen <ericleonen@gmail.com>
2025-06-14 21:30:22 -0700

    restructure project

--- data.py
+++ data.py
@@ -1,8 +1,25 @@
-from data.fred_client import fred
+from fred_client import fred
 import pandas as pd
 import numpy as np
-from data.helpers import get_recessions
 
+def get_recessions(start_date: str | pd.Timestamp) -> np.ndarray:
+    """
+    Returns a 2-wide ndarray of recessionary periods starting at the given start date. Each row
+    provides the start and end dates of the recession as per NBER standards.
+    """
+    usrec = fred.get_series("USREC")[start_date:]
+    usrec = usrec.asfreq("MS")
+
+    usrec_prev = usrec.shift(1)
+    starts = list(usrec[(usrec == 1) & (usrec_prev == 0)].index)
+    ends = list(usrec[(usrec == 0) & (usrec_prev == 1)].index)
+
+    if len(ends) < len(starts):
+        ends.append(usrec.index[-1])
+    elif len(ends) > len(starts):
+        starts.append(usrec.index[0])
+
+    return np.vstack([starts, ends]).T
 
 class RecessionDatasetBuilder:
     """
